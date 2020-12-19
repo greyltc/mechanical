6eb9f87e4862abbb66c2dece3bbdf9b4ed8c4263
Greyson Christoforo <grey@christoforo.net>
2020-12-19 00:18:01 +0000

    scratcher bottom

--- scratch_tool/scratch-tool.py
+++ scratch_tool/scratch-tool.py
@@ -8,7 +8,16 @@ class ScratchTool(object):
     extra_xy = 30
     glass_dim = 30
     bottom_z = 5
-    glass_xy_fudge = 0.5;
+    glass_xy_fudge = 0.5
+
+    dowel_nominal_d = 3
+    dowel_press_fudge = 0.2
+
+    dowel_offset_from_center = 4.3306
+    nominal_glass_pin_play = 0.2
+
+    bottom_safe_step_z = 0.3
+    bottom_safe_step_xy = 28
 
     def __init__(self, extra_xy=30, glass_dim=30):
         self.extra_xy = extra_xy
@@ -23,19 +32,23 @@ class ScratchTool(object):
         x = pocket_xy + s.extra_xy
         y = pocket_xy + s.extra_xy
 
-        # make the spacer base layer
+        dowel_press_d = s.dowel_nominal_d + s.dowel_press_fudge
+
+        # make the bottom piece
         bottom = cq.Workplane("XY")
         bottom = bottom.box(x, y, s.bottom_z, centered=(True, True, False))
+        bottom = bottom.faces("<Z").workplane(centerOption="CenterOfBoundBox").rarray(s.glass_dim+s.dowel_nominal_d, 2*s.dowel_offset_from_center, 2, 2).hole(dowel_press_d)
+        bottom = bottom.faces("<Z").workplane(centerOption="CenterOfBoundBox").rarray(2*s.dowel_offset_from_center, s.glass_dim+s.dowel_nominal_d, 2, 2).hole(dowel_press_d)
+        bottom = bottom.faces(">Z").rect(s.bottom_safe_step_xy, s.bottom_safe_step_xy).cutBlind(s.bottom_safe_step_z)
+
         assembly.extend(bottom.vals())
 
         cpnd = cq.Compound.makeCompound(assembly)
 
         return cpnd
 
-
 def make_demo_solids():
     s = ScratchTool(extra_xy=30, glass_dim=30)
-    s = Sandwich(leng=166, wid=50, substrate_xy=30, cutout_spacing=35, endblock_width=12, aux_hole_spacing=16, block_offset_from_edge_of_base=1)
     cmpd = s.build()
     return cmpd.Solids()
 
@@ -44,6 +57,7 @@ def make_steps():
     for salad in salads:
         this_hash = salad.hashCode()  # this might not be unique because it does not hash orientation
         tb.utilities.export_step(salad, pathlib.Path(f"{this_hash}.step"))
+        cq.exporters.export(salad,f"{this_hash}.stl")
 
 if "show_object" in locals():
     salads = make_demo_solids()
