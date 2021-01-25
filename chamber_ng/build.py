74f7fbfa97ba30ef1880abbae1ae99aef97e2c20
Greyson Christoforo <grey@christoforo.net>
2021-01-25 15:49:51 +0000

    improve drawing algos

--- chamber_ng/build.py
+++ chamber_ng/build.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
-import cadquery as cq
-from cadquery.cq import Workplane
+import cadquery
+#import cadquery as cq
+from cadquery.cq import CQ, Workplane
 import geometrics.toolbox as tb
 import logging
 
 
 class ChamberNG(object):
-    # -X, +X, -Y, +Y (slots are in -Y wall)
-    wall = (10, 10, 10, 10)
+    # wall thicknesses
+    wall = (10, 10, 10, 10)  # -X, +X, -Y, +Y (slots are in -Y wall)
+
+    # nominal pcb thickness
     pcb_thickness = 1.6
 
     # (normal plane) clearance given around the crossbar PCBs when cutting their slots in the wall
-    pcb_slot_clearance = pcb_thickness*0.10/2  # covers a PCB that's 10 percent too thick
-
-    # the height above the floor(shelf) that the endblocks hold the bottom edge of the crossbar PCBs
-    #pcb_z_gap = 0.2
-
-    # width of the hole in the wall
-    #in_wall_void_width = 4
-    #in_wall_void_extra_depth = 2  # further distance below shelf level
+    pcb_slot_clearance = pcb_thickness*0.10  # covers a PCB that's 10 percent too thick
 
     endblock_wall_spacing = 1  # from + and - Y walls
     endblock_thickness = 12
-
-    # add this much extra room in x and y
-    # for x, at least 1.4576 is a requirement to prevent shadowing when spacing = 0 and shelf_height=5
-    # for y, at least 5.1154 is a requirement to prevent shadowing when spacing = 0 and shelf_height=5 
-    extra_x = (2*pcb_slot_clearance + pcb_thickness)  # makes sense when spacing = 0
-    extra_y = 6
+    
+    # extra room between the device plane and the walls on each side: (-X, +X, -Y, +Y)
+    # these must be at least 0.7288 to prevent shadowing when spacing = 0 and shelf_height=5
+    x_minus = pcb_slot_clearance + pcb_thickness/2 # makes sense when spacing = 0
+    x_plus = pcb_slot_clearance + pcb_thickness/2 # makes sense when spacing = 0
+    # these must be at least 2.5577 to prevent shadowing when yspacing = 0 and shelf_height=5
+    y_minus = 3
+    y_plus = 3
+    extra = (x_minus, x_plus, y_minus, y_plus)
 
     # adds features on the sides of the shelves to ensure the PCBs get jammed up against their endblocks
     # only really makes sense when extra_x > 0 and x spacing = 0
@@ -38,7 +37,6 @@ class ChamberNG(object):
     top_mid_height = 5
 
     # constants for the slot-side potting pocket
-    potting_pocket_from_edges = [7+extra_x/2, 7+extra_x/2, top_mid_height-2, shelf_height-2]  # edge order: [+X, -X, +Z, -Z]
     potting_pocket_depth = 2
     potting_pocket_fillet = 2
 
@@ -52,122 +50,144 @@ class ChamberNG(object):
         logging.basicConfig(format='%(message)s')
         self.log = logging.getLogger(__name__)
 
-    def make_middle(self):
+    def make_plane(self):
+        """makes the plane where the devices will be"""
+        s = self
+        planex = s.array[0]*(s.substrate_adapters[0]+s.substrate_spacing[0])
+        planey = s.array[1]*(s.substrate_adapters[1]+s.substrate_spacing[1])
+        werkplane = CQ().rect(planex, planey)
+        return werkplane
+
+    def make_middle(self, wp):
         s = self
         co = "CenterOfBoundBox"
-        extents = [
-            s.wall[0]+s.wall[1]+s.substrate_spacing[0]+s.array[0]*(s.substrate_adapters[0]+s.substrate_spacing[0]) + s.extra_x,
-            s.wall[2]+s.wall[3]+s.substrate_spacing[1]+s.array[1]*(s.substrate_adapters[1]+s.substrate_spacing[1])+2*(s.endblock_thickness+s.endblock_wall_spacing) + s.extra_y
-        ]
-        # if we're doing 100% packing in X, leave some room on the outside edges for screw heads and connectors
-        if (s.substrate_spacing[0] == 0):
-            if s.extra_x < (2*self.pcb_slot_clearance + self.pcb_thickness):
-                s.log.warning('Slots will be cut into the side walls')
+        
+        if (s.substrate_spacing[0]/2 + s.extra[0]) <  (2*self.pcb_slot_clearance + self.pcb_thickness)/2:
+            s.log.warning('Slots will be cut into the inner -X wall')
+        
+        if (s.substrate_spacing[0]/2 + s.extra[1]) <  (2*self.pcb_slot_clearance + self.pcb_thickness)/2:
+            s.log.warning('Slots will be cut into the inner +X wall')
 
-        shelf_width = s.endblock_wall_spacing+s.endblock_thickness
-
-        mid_void_extents = [extents[0]-s.wall[0]-s.wall[1], extents[1]-s.wall[2]-s.wall[3]-2*shelf_width]
-        shelf_void_extents = [extents[0]-s.wall[0]-s.wall[1]-s.extra_x, extents[1]-s.wall[2]-s.wall[3]]
+        shelf_width = s.endblock_wall_spacing + s.endblock_thickness
 
         if self.use_shelf_PCB_jammers == False:
-            shelf_void_extents[0] = shelf_void_extents[0] + s.extra_x
+            # TODO
+            pass
+            #shelf_void_extents[0] = shelf_void_extents[0] + s.extra_x
+        
+        # werkplane face
+        wpf = cadquery.Face.makeFromWires(wp.val())
 
+        # to get the dims of the working array
+        wpbb = wpf.BoundingBox()  
 
-        # the base "ring"
-        middle = (
-            cq.Workplane('XY')
-            .box(extents[0], extents[1], s.wall_height, centered=[True,True,False])
-            .translate([(s.wall[1]-s.wall[0])/2,(s.wall[3]-s.wall[2])/2,0])
-            .moveTo(0,0)
-            .rect(mid_void_extents[0], mid_void_extents[1], centered=True)
-            .cutThruAll()
-            .moveTo(0,0)
-            .rect(shelf_void_extents[0], shelf_void_extents[1], centered=True)
-            .cutThruAll()
+        # the void under the device array
+        av = wp.extrude(-s.wall_height)
+
+        # the void under the device array plus the extra spacing values
+        av_ex = CQ(av.findSolid())
+        # need the if checks because extrude(0) chrashes
+        if s.extra[0] > 0:  # add in -X direction
+            av_ex = CQ(av_ex.findSolid()).faces("<X[-1]").wires().toPending().workplane().extrude(s.extra[0])
+        if s.extra[1] > 0:  # add in -X direction
+            av_ex = CQ(av_ex.findSolid()).faces(">X[-1]").wires().toPending().workplane().extrude(s.extra[1])
+        if s.extra[2] > 0:  # add in -X direction
+            av_ex = CQ(av_ex.findSolid()).faces("<Y[-1]").wires().toPending().workplane().extrude(s.extra[2])
+        if s.extra[3] > 0:  # add in -X direction
+            av_ex = CQ(av_ex.findSolid()).faces(">Y[-1]").wires().toPending().workplane().extrude(s.extra[3])
+        av_ex_rect = av_ex.faces(">Z[-1]").wires()  # the rectangle for that
+
+        # the void under the device array plus the extra spacing values plus the shelf space
+        av_ex_s = (
+            CQ(av_ex.findSolid())
+            .faces("<Y[-1]").wires().toPending().workplane().extrude(shelf_width)  # add in -Y direction
+            .faces(">Y[-1]").wires().toPending().workplane().extrude(shelf_width)  # add in +Y direction
         )
 
-        # add the shelf that holds the endblocks
-        _shelf = (
-            cq.Workplane('XY')
-            .rect(extents[0], extents[1], centered=True)
-            .extrude(-s.shelf_height)
-            .translate([(s.wall[1]-s.wall[0])/2, (s.wall[3]-s.wall[2])/2,0])
-            .moveTo(0,0)
-            .rect(mid_void_extents[0], mid_void_extents[1], centered=True)
-            .cutThruAll()
-            )
-        middle = middle.union(_shelf)
+        # the ring extents box, including the shelves
+        extents_box = (
+            CQ(av_ex_s.findSolid())
+            # the extra for the walls
+            .faces("<X[-1]").wires().toPending().workplane().extrude(s.wall[0])  # add in -X direction
+            .faces(">X[-1]").wires().toPending().workplane().extrude(s.wall[1])  # add in +X direction
+            .faces("<Y[-1]").wires().toPending().workplane().extrude(s.wall[2])  # add in -Y direction
+            .faces(">Y[-1]").wires().toPending().workplane().extrude(s.wall[3])  # add in +Y direction
+            # add the shelf chunk
+            .faces("<Z[-1]").wires().toPending().workplane().extrude(s.shelf_height)  # add in -Z direction
+        )
+
+        # cut the void above the shelf, then cut the extended window to make the shelved ring shape
+        shelved_ring = (
+            extents_box.cut(av_ex_s)
+            .add(av_ex_rect).toPending().cutThruAll()
+        )
 
         # non-slot side shelf mounting block holes
-        middle = (
-            middle.faces('<Z')
-            .workplane(origin=(0,0,4))
-            .center(0, -mid_void_extents[1]/2-s.endblock_thickness/2)
+        shelved_ring = (
+            shelved_ring.faces('+Z').faces('>Y').workplane(centerOption=co, invert=True, offset=s.shelf_height)
+            .center((s.extra[0]-s.extra[1])/2, +shelf_width/2-s.endblock_thickness/2)  # invert=True flips Y but not X
             .rarray(xSpacing=(s.period[0]), ySpacing=1, xCount=s.array[0], yCount=1)
             .cskHole(**tb.c.csk('m5', 11))
         )
 
         # slot side shelf mounting block holes
-        middle = (
-            middle.faces('<Z')
-            .workplane(origin=(0,0,4))
-            .center(0, mid_void_extents[1]/2+s.endblock_thickness/2)
+        shelved_ring = (
+            shelved_ring.faces('+Z').faces('<Y').workplane(centerOption=co, invert=True, offset=s.shelf_height)
+            .center((s.extra[0]-s.extra[1])/2, -shelf_width/2+s.endblock_thickness/2)  # invert=True flips Y but not X
             .rarray(xSpacing=(s.period[0]), ySpacing=1, xCount=s.array[0], yCount=1)
             .cskHole(**tb.c.csk('m5', 11))
         )
 
-        # the top-mid piece
-        _top_mid = (
-            cq.Workplane('XY')
-            .rect(extents[0], extents[1], centered=True).extrude(s.top_mid_height)
-            .translate([(s.wall[1]-s.wall[0])/2,(s.wall[3]-s.wall[2])/2, s.wall_height])
-            .moveTo(0,0)
-            .rect(extents[0]-s.wall[0]-s.wall[1], extents[1]-s.wall[2]-s.wall[3], centered=True)
-            .cutThruAll()
+        # add on the material for the top-mid piece
+        shelved_ring = (
+            CQ(shelved_ring.findSolid())
+            .faces(">Z[-1]").wires().toPending().extrude(s.top_mid_height)  # add in +Z direction
         )
-        middle = middle.union(_top_mid)
 
-        # a construction tool for subtracting the PCB slots
+        c_len = wpbb.ylen + 2*shelf_width + s.extra[2] + s.extra[3] + s.wall[2] + s.wall[3]  # length of the slot cutter
         _pcb_slot_void = (
-            cq.Workplane('XY')
+            CQ()
+            .workplane(offset=-s.wall_height)
             .rarray(xSpacing=(s.period[0]), ySpacing=1, xCount=s.array[0], yCount=1)
-            .box(s.pcb_thickness+2*s.pcb_slot_clearance, extents[1], s.wall_height, centered=[True, False, False])
-            .translate([0, -extents[1]+mid_void_extents[1]/2+shelf_width, 0]) # so that the slots are cut in the -Y wall
-            )
-        middle = middle.cut(_pcb_slot_void.translate([-s.substrate_adapters[0]/2,0,0]))
-        middle = middle.cut(_pcb_slot_void.translate([ s.substrate_adapters[0]/2,0,0]))
-
-        # make the slot side potting pocket
-        _side_potting_pocket = (
-            cq.Workplane('XY')
-            .box(extents[0], extents[1], s.wall_height+s.shelf_height+s.top_mid_height, centered=[True, False, False])
-            .translate([0, -extents[1]-mid_void_extents[1]/2-shelf_width-s.wall[2]+s.potting_pocket_depth, -s.shelf_height])
-            .faces('>X[-1]').workplane(-s.potting_pocket_from_edges[0]).split(keepBottom=True)
-            .faces('<X[-1]').workplane(-s.potting_pocket_from_edges[1]).split(keepBottom=True)
-            .faces('>Z[-1]').workplane(-s.potting_pocket_from_edges[2]).split(keepBottom=True)
-            .faces('<Z[-1]').workplane(-s.potting_pocket_from_edges[3]).split(keepBottom=True)
-            .edges().fillet(s.potting_pocket_fillet)
+            .box(s.pcb_thickness+2*s.pcb_slot_clearance, c_len, s.wall_height, centered=[True, False, False])
+            .translate([0, -c_len+wpbb.ymax+s.extra[3]+s.endblock_thickness, 0]) # so that the slots are cut in the -Y wall
         )
-        middle = middle.cut(_side_potting_pocket)
-        #potting_pocket_from_edges = [2, 2, 2, 2]  # edge order: [+X, -X, +Z, -Z]
 
-        top = middle.faces('>Z[-1]').workplane(-s.top_mid_height).split(keepTop=True)
-        middle = middle.faces('>Z[-1]').workplane(-s.top_mid_height).split(keepBottom=True)
+        shelved_ring = (
+            CQ(shelved_ring.findSolid())
+            .cut(_pcb_slot_void.translate([-s.substrate_adapters[0]/2,0,0]))
+            .cut(_pcb_slot_void.translate([ s.substrate_adapters[0]/2,0,0]))
+        )
+
+        side_pot_cutter = (
+            CQ()
+            .box(wpbb.xlen+2*s.potting_pocket_fillet+s.pcb_thickness+2*s.pcb_slot_clearance, 4*s.potting_pocket_depth, s.wall_height+2*s.potting_pocket_fillet, centered=[True, False, False])
+            .translate([0, -4*s.potting_pocket_depth+wpbb.ymin-s.extra[2]-shelf_width-s.wall[2]+s.potting_pocket_depth, -s.wall_height-s.potting_pocket_fillet])
+            .edges().fillet(s.potting_pocket_fillet)
+            #.edges().chamfer(s.potting_pocket_fillet)
+        )
+        shelved_ring = shelved_ring.cut(side_pot_cutter)
+
+
+        top = shelved_ring.faces('>Z[-1]').workplane(-s.top_mid_height).split(keepTop=True)
+        middle = shelved_ring.faces('>Z[-1]').workplane(-s.top_mid_height).split(keepBottom=True)
 
 
         return (middle, top)
 
-        #middle = cq.Workplane('ZX').add(middle.findSolid()).faces('>Y').wires().toPending().extrude(8)
-
 
     def build(self):
         s = self
-        asy = cq.Assembly()
+        asy = cadquery.Assembly()
+
+        # the device plane
+        werkplane = self.make_plane()
+        asy.add(werkplane, name="werkplane")
 
         # make the middle piece
-        middle, top_mid = self.make_middle()
-        asy.add(middle, name="middle", color=cq.Color("orange"))
-        asy.add(top_mid, name="top_mid", color=cq.Color("yellow"))
+        middle, top_mid= self.make_middle(werkplane)
+        asy.add(middle, name="middle", color=cadquery.Color("orange"))
+        asy.add(top_mid, name="top_mid", color=cadquery.Color("yellow"))
 
 
         # make the top piece
@@ -183,7 +203,7 @@ class ChamberNG(object):
         return asy
 
 def main():
-    s = ChamberNG(array=(1, 1), subs =(30, 30), spacing=(0, 0))
+    s = ChamberNG(array=(4, 4), subs =(30, 30), spacing=(10, 10))
     asy = s.build()
     
     if "show_object" in globals():
@@ -191,7 +211,7 @@ def main():
         for key, val in asy.traverse():
             shapes = val.shapes
             if shapes != []:
-                c = cq.Compound.makeCompound(shapes)
+                c = cadquery.Compound.makeCompound(shapes)
                 odict = {}
                 if val.color is not None:
                     co = val.color.wrapped.GetRGB()
@@ -207,7 +227,7 @@ def main():
         for key, val in asy.traverse():
             shapes = val.shapes
             if shapes != []:
-                c = cq.Compound.makeCompound(shapes)
-                cq.exporters.export(c, f'{val.name}.stl')
+                c = cadquery.Compound.makeCompound(shapes)
+                cadquery.exporters.export(c, f'{val.name}.stl')
 
 main()
